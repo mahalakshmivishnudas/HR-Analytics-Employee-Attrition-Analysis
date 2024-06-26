da8665cf6f5b9ce52e2ec53804252430dfcc4e49
Mahalakshmi Vishnudas <mahalakshmivishnudas@gmail.com>
2024-06-26 21:16:52 +0530

    HR ANALYTICS SQL CODE.sql

--- HR ANALYTICS SQL CODE.sql
+++ HR ANALYTICS SQL CODE.sql
@@ -5,67 +5,31 @@ SELECT * FROM hrdata
 -- 1.Calculate Total Employee Count 
 SELECT COUNT(employee_count) as employee_count FROM hrdata
 
-
 -- 2.Calculate Attrition Count
 SELECT COUNT(attrition) as attrition_count FROM hrdata
 WHERE attrition = 'Yes'
-
-
-
-
-
-
-
-
-
-
-
-
 
 -- 3.Calculate Attrition rate
 SELECT CAST(ROUND((CAST(SUM(CASE WHEN (attrition) = 'Yes' THEN 1 ELSE 0 END) AS DECIMAL(10, 2)) *100 
 / CAST(COUNT(*) AS DECIMAL(10, 2))), 2) AS DECIMAL(10, 2)) AS Employee_attritionrate
 FROM hrdata
 
-
-
-
-
-
--- 4.Calculate Active Employees
+  -- 4.Calculate Active Employees
 SELECT SUM(employee_count) - (select count(attrition) from hrdata where (attrition) = 'Yes')
 from hrdata
-
-
-
-
-
-
 
 -- 5.Calculate Average Age
 SELECT (ROUND(CAST(AVG(Coalesce(age, 0.0)) AS DECIMAL(10, 0)), 0))  AS Avg_Age from hrdata
 
 
 
-
-
-
-
-
-
---Chart's Requirements
+  --Chart's Requirements
 
 --1.Calculate Attrition By Gender
 SELECT COUNT(attrition) AS Attrition_By_Gender, gender FROM hrdata 
 WHERE (attrition) = 'Yes'
 GROUP BY gender
 ORDER BY COUNT(attrition) DESC
-
-
-
-
-
-
 
 
 --2.Calculate Department Wise Attrition
@@ -77,15 +41,10 @@ GROUP BY department
 ORDER BY COUNT(attrition) DESC
 
 
-
 --3.Calculate No.of Employee by Age Group
 SELECT age, SUM(employee_count) AS Employee_Count FROM hrdata
 GROUP BY age
 ORDER BY age
-
-
-
-
 
 
 --4.Calculate Education Field Wise Attrition
@@ -94,7 +53,8 @@ WHERE (attrition) = 'Yes'
 GROUP BY education_field
 ORDER BY COUNT(attrition) DESC
 
---5.Calculate Attrition Rate by Gender for Different AgeGroup
+
+  --5.Calculate Attrition Rate by Gender for Different AgeGroup
 SELECT age_band, gender, COUNT(attrition) as attrition_count, 
 ROUND((CAST(COUNT(attrition) AS NUMERIC) / 
 (SELECT COUNT(attrition) FROM hrdata WHERE attrition = 'Yes'))*100, 2) AS Percentage 
